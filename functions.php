121c40c12fb7a57584d8845959d2af133e99be88
Pochon Christophe <chris26-78@hotmail.fr>
2023-10-04 11:59:03 +0200

    insercetion essaie back end post+taxomanie

--- functions.php
+++ functions.php
@@ -19,7 +19,76 @@ function test_variable($valeur) {       tester une variable
 }
 add_filter('la fonction ou la houk s insere','test_variable');
 */
+function cuisine_init(){
+    register_taxonomy('type_plats','recette',[
+        'labels'=> [
+            'name'=> 'type de plat',
+            'edit_items'=>'tous les type de plat',
+           'add_new_item'=>'Ajouter type de plat',
+        ],
+        'show_in_rest' => true,
+        'hierarchical' => true,
+    ]);
 
+
+
+    register_taxonomy('type_aliment','ingredient',[
+        'labels'=> [
+            'name'=> 'type aliment',
+            'edit_items'=>'tous les type aliment',
+           'add_new_item'=>'Ajouter type aliment',
+        ],
+        'show_in_rest' => true,
+        'hierarchical' => true ,
+    ]);
+
+    register_taxonomy('parfum','recette',[
+        'labels'=> [
+            'name'=> 'Parfum',
+            'edit_items'=>'tous les type parfum',
+           'add_new_item'=>'Ajouter type parfum',
+        ],
+        'show_in_rest' => true,
+        'hierarchical' => true ,
+    ]);
+
+    register_taxonomy('specialite','recette',[
+        'labels'=> [
+            'name'=> 'specialité',
+            'edit_items'=>'tous les type specialité',
+           'add_new_item'=>'Ajouter un pays ou une region',
+        ],
+        'show_in_rest' => true,
+        'hierarchical' => true ,
+    ]);
+
+    register_post_type('recette',[
+        'label' => 'Recettes',
+        'public'=>true,
+        'menu_position'=> 2,
+        'menu_icon'=>'dashicons-welcome-write-blog',
+        'supports'=>['title','editor','thumbnail'],
+       'show_in_rest' => true,
+
+    ]);
+    register_post_type('ingredient',[
+        'label' => 'Ingredients',
+        'public'=>true,
+        'menu_position'=> 2,
+    'menu_icon'=>'dashicons-cart',
+    'supports'=>['title','editor','thumbnail'],
+    'show_in_rest' => true,
+    ]);
+
+
+}
+
+function cuisine_add_custum_box(){
+
+}
+
+add_action('init','cuisine_init');
 add_action('after_setup_theme','theme_cuisine');
 add_action('wp_enqueue_scripts','theme_cuisine_assets');
+add_action('add_meta_boxes','cuisine_add_custum_box');
 
